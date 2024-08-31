9053f458f7a948cfd64a5006e9c64fdcd3474685
Clark Ren <renc@rose-hulman.edu>
2024-08-31 16:37:55 -0700

    wrote firebase script

---
+++ firebaseTest.py
@@ -0,0 +1,13 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import json
+
+cred = credentials.Certificate('plantmonitor-19a40-firebase-adminsdk-8b656-ec89234839.json')
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://plantmonitor-19a40-default-rtdb.firebaseio.com'})
+# firebase_admin.initialize_app(cred)
+ref = db.reference("/")
+
+with open("test.json", "r") as f:
+	file_contents = json.load(f)
+ref.set(file_contents)
