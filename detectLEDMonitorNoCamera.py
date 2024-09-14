3c4757438dc3d4b17c1e7878564f22dc3f467761
Clark Ren <renc@rose-hulman.edu>
2024-09-14 16:21:15 -0700

    auth broke, fixed

--- detectLEDMonitorNoCamera.py
+++ detectLEDMonitorNoCamera.py
@@ -11,7 +11,7 @@ from firebase_admin import credentials
 from firebase_admin import db
 import json
 
-cred = credentials.Certificate('plantmonitor-19a40-firebase-adminsdk-8b656-ec89234839.json')
+cred = credentials.Certificate('plantmonitor-19a40-firebase-adminsdk-8b656-f5000f8953.json')
 firebase_admin.initialize_app(cred, {'databaseURL': 'https://plantmonitor-19a40-default-rtdb.firebaseio.com'})
 # firebase_admin.initialize_app(cred)
 ref = db.reference("/")
